7d233adc6c595a04154050f0a4f133b7011949b3
windsnow1025 <windsnow1025@gmail.com>
2026-04-08 00:09:40 -0400

    Create CallableTest.java

---
+++ app/src/main/java/concurrency/CallableTest.java
@@ -0,0 +1,32 @@
+package concurrency;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+
+public class CallableTest {
+    static void main() throws Exception {
+        MyThread mt = new MyThread();
+
+        FutureTask<Integer> result = new FutureTask<>(mt);
+
+        new Thread(result).start();
+
+        Integer sum = result.get();
+
+        System.out.println(sum);
+    }
+}
+
+class MyThread implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
